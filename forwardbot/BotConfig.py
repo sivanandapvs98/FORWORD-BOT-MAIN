7a8cab98f5d25cc13c73e3821da1c9118be9d01a
sivanandapvs98 <sivananda.p.v.s@gmail.com>
2024-07-01 14:31:28 +0530

    Update BotConfig.py

--- forwardbot/BotConfig.py
+++ forwardbot/BotConfig.py
@@ -3,7 +3,7 @@ class Config(object):
     API_ID = environ.get("API_ID", "29868868")
     API_HASH = environ.get("API_HASH", "6b7bd10846ff6d7e8f50a4bfe13c9fd4")
     BOT_TOKEN = environ.get("BOT_TOKEN", "7014461118:AAESAgPjfHgMwK6tEyCqWPuiQK1kgvUuGXY")
-    STRING_SESSION = environ.get("STRING_SESSION", "1BVtsOGoBu2qLbRUJOUdqsPA9MhEDNntzqgc8wbXV5o64_8DboVFFt8vDCHerEBG_cI0FPcpevCnHTl2HOhTyZPNoxbSeUxnnu0Pt-ebx4mplxfOEkE74QoACGjqTR_SReVMQ7Ozv3eUciyri-erhSISE9a4CB6wiT66oMwtQZsSW2YLT3LiFokFylf8pI0qeunOzK1qbRkof9BL43o-Qj0ykCMzql8MQXwp0aMt7-WhtvHPMz3VX9TYQ4jdrAsSI2sa6YukDpBShz4tSflhIVR78xJmwk_Ej6_BS_G4LFrHfiZr78WQilPzxKDyv9utCPAVWzLbpzwCnEAoJerMppMmOpPpUFd4=")
+    STRING_SESSION = environ.get("STRING_SESSION", "1BVtsOHcBu5Q88zlHBcGYdIlDF7-V66t6eNegXnsYpxcs8P1LHfyMCR8dsKayGnm79TTxlftCfxGeWOlZi60Ku-rgwaNT2YjQnTox1GjXPk6iFujJvO1d80wq-bE9ozNv2s2UJNj3HX2AHwNl5krO3xgv0Y8AOesaJu0Gw8CjHesaEQ0Z4a2IbIqaJ3Lzcp_cg83vYGA2aUSxK-PaCS5PLhUq0d3Bn0F9CCmMY49d3HX9XcvYhJjlFvLrIKn8tWZhBf9K0bH0bfspumJwZM5yokL8T1CqTTGim9Yolm1VvJiPWyA8UwW4VDaRSmQ13Eje4NchrBdjdlvytzlq-nCrNME8ezSGNzg=")
     SUDO_USERS = environ.get("SUDO_USERS", "1408595569 786843013")
     COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "^/")
     HELP_MSG = """
